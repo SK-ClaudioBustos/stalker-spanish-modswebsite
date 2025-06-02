85d0d146d60e5a3a02c5a8170c3bad4f9c7cfdd0
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-06-02 20:32:17 -0300

    feat: se añadio un archivo con informacion sobre los juegos de la franquicia

---
+++ src/data/games.ts
@@ -0,0 +1,24 @@
+import SOC from "@soc/portrait.webp";
+import CS from "@cs/portrait.webp";
+import COP from "@cop/portrait.webp";
+
+export const GAMES = [
+  {
+    year: "2007",
+    title: "Stalker Shadow of Chernobyl",
+    imgPath: SOC,
+    url: "/mods/stalker/shadow-of-chernobyl"
+  },
+  {
+    year: "2008",
+    title: "Stalker Clear Sky",
+    imgPath: CS,
+    url: "/mods/stalker/clear-sky"
+  },
+  {
+    year: "2009",
+    title: "Stalker Call of Prypiat",
+    imgPath: COP,
+    url: "/mods/stalker/call-of-pripyat"
+  },
+];
