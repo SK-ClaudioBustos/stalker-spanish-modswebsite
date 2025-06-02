576369cf0f101094a7e0a8261f8109904e1c47de
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-06-02 15:42:34 -0300

    feat: se cambio de ubicacion la informacion de las aclaraciones

--- src/section/AclaracionesSection/AclaracionesSection.tsx
+++ src/section/AclaracionesSection/AclaracionesSection.tsx
@@ -1,30 +1,7 @@
 import { TitleContainer } from "@component/ui/TitleContainer";
 import "./AclaracionesSection.css";
 import Title from "@assets/img/definiciones_aclaraciones_title.webp";
-import Background1 from "@backgrounds/background_1.webp";
-import Background2 from "@backgrounds/background_2.webp";
-import Background3 from "@backgrounds/background_3.webp";
-
-const ACLARACIONES = [
-  {
-    imgSrc: Background1,
-    titulo: "Vanilla + MOD",
-    aclaracion:
-      "Estos mods no alteran la historia en absoluto, sólo mejoran la mecánica de juego y tal vez los efectos visuales. Ideal para los que llevan mucho tiempo sin rejugar la saga y buscan una experiencia diferente.",
-  },
-  {
-    imgSrc: Background2,
-    titulo: "MODS de Historia",
-    aclaracion:
-      "Mods que buscan alterar la historia de uno de los juegos o escribir la suya propia, simplemente un mod impulsado por la historia. Ideal para los que buscan experiencias frescas dentro del universo Stalker. En este apartado se desprenden grandes sagas y excelentes historias.",
-  },
-  {
-    imgSrc: Background3,
-    titulo: "MODS Freeplays",
-    aclaracion:
-      "Mods sin historia, sandbox centrados en el gameplay y la guerra de facciones. Estos suelen ser los mas populares en la comunidad (llegando a fragmentarla). De todas formas es ideal para los que solo buscan la experiencia de sobrevivir en la Zona y crear su propia historia.",
-  },
-];
+import { ACLARACIONES } from "@data/aclaraciones";
 
 export const AclaracionesSection = () => {
   return (
