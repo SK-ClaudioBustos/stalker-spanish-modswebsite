42d61a44ed11bc7d5490a549a0d69e086fd3c9ac
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-06-13 15:10:29 -0300

    style: change style in grid for enhance presentation

--- src/components/ModsList/ModsGrid/ModsGrid.tsx
+++ src/components/ModsList/ModsGrid/ModsGrid.tsx
@@ -7,7 +7,7 @@ interface GameGridItems {
 
 export const ModsGrid = ({ items }: GameGridItems) => {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] justify-items-center gap-x-0 gap-y-7">
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] justify-items-center gap-x-0 gap-y-7">
       {items.map((item, index) => (
         <ModItem item={item} key={index} />
       ))}
