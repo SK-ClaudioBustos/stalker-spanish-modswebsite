15ccfbc4ed303a0e44c30aff977689ef86e78ba6
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-08-25 10:21:11 -0300

    feaet: update codegen configuration and update dependencies

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,25 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:3000/graphql",
+  schema: 'http://localhost:3000/graphql',
   documents: "src/graphql/**/*.ts",
   generates: {
     "src/generated/graphql.ts": {
-      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo']
-    }
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      config: {
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+        withRefetchFn: true
+      },
+    },
   },
-  ignoreNoDocuments: true
+  ignoreNoDocuments: false,
 };
 
 export default config;
